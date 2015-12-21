eb7518a83b1e5eef317032b568b004f3e34d5a2a
Jim Mackin <jim.mackin@salesagility.com>
2015-12-21 12:10:22 +0000

    Add day 11

---
+++ day11-2.php
@@ -0,0 +1,39 @@
+<?php
+
+echo "Next pass is ".getNextPass('vzbxxyzz')."\n";
+
+function getNextPass($current){
+    //Lazy solution - loop over each and check it's valid
+    $current++;
+    while(!valid($current)){
+
+        $current++;
+    }
+    return $current;
+}
+
+function valid($pass){
+    if(preg_match('/i|o|l/',$pass)){
+        return false;
+    }
+    if(!preg_match('/(.)\1.*(.)\2/',$pass)){
+        return false;
+    }
+    $prev = -1;
+    $straightLen = 1;
+    for($x = 0; $x < strlen($pass); $x++){
+        $cur = substr($pass,$x,1);
+        $prev++;
+
+        if($cur == $prev){
+            $straightLen++;
+            if($straightLen >= 3){
+                return true;
+            }
+        }else{
+            $straightLen = 1;
+        }
+        $prev = $cur;
+    }
+    return false;
+}
