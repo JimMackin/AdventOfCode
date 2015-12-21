d55f2043e76aa93291097f17c3746fa12d2473d0
Jim Mackin <jim.mackin@salesagility.com>
2015-12-21 12:08:06 +0000

    Add day 10

---
+++ day10-1.php
@@ -0,0 +1,29 @@
+<?php
+$say = "1113122113";
+for($x = 0; $x < 40; $x++){
+    $say = lookAndSay($say);
+}
+echo strlen($say)."\n";
+
+function lookAndSay($str){
+    $prevDigit = -1;
+    $runs = 0;
+    $out = '';
+    for($x = 0; $x < strlen($str); $x++){
+        $digit = substr($str,$x,1);
+        if($prevDigit == -1){
+            $runs++;
+            $prevDigit = $digit;
+            continue;
+        }
+        if($prevDigit == $digit){
+            $runs++;
+        }else{
+            $out .= $runs . $prevDigit;
+            $runs = 1;
+        }
+        $prevDigit = $digit;
+    }
+    $out .= $runs . $prevDigit;
+    return $out;
+}
